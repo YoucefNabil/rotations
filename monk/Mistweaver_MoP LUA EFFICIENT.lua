379d01a12978968772908bcc6cabe459345029a4
YoucefNabil <pigrelin33@gmail.com>
2024-05-12 12:37:57 +0100

    flame shock dispell

--- monk/Mistweaver_MoP LUA EFFICIENT.lua
+++ monk/Mistweaver_MoP LUA EFFICIENT.lua
@@ -1193,7 +1193,7 @@ local mw_rot = {
 							and not fr:DebuffAny("Unstable Affliction")
 							and (fr:DebuffType("Magic") or fr:DebuffType("Poison") or fr:DebuffType("Disease")) then
 							if fr:State("fear || sleep || charm || disorient || incapacitate || misc || stun || root || silence") or fr:LostControl() or _A.pull_location == "party" or _A.pull_location == "raid"
-								or fr:DebuffAny("Entangling Roots") or fr:DebuffAny("Freezing Trap") or fr:DebuffAny("Denounce")
+								or fr:DebuffAny("Entangling Roots") or fr:DebuffAny("Freezing Trap") or fr:DebuffAny("Denounce") or fr:DebuffAny("Flame Shock")
 								then
 								if fr:los() then
 									return fr:Cast("Detox")
